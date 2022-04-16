3b085db075b75a42f50ce66ec39ef8e637dd5d9f
severingafner <severingafner@gmail.com>
2022-04-16 14:31:52 +0200

    Adjust the Navbar component to display a navbar (without linking)

--- property-management/components/layout.tsx
+++ property-management/components/layout.tsx
@@ -1,11 +1,28 @@
-import Navbar from './navbar'
+import Navbar, { NavbarProps } from './navbar'
+import PersonIcon from '@mui/icons-material/Person';
+import OtherHousesIcon from '@mui/icons-material/OtherHouses';
 interface LayoutProps {
     children: React.ReactNode;
  }
 export default function Layout({ children }: LayoutProps) {
+  const navbar: NavbarProps = {
+    title: 'Property Manager',
+    items: [
+      {
+        text: 'Persons',
+        link: '',
+        Icon: PersonIcon
+      },
+      {
+        text: 'Properties',
+        link: '',
+        Icon: OtherHousesIcon
+      },
+    ]
+  };
   return (
     <>
-      <Navbar />
+      <Navbar title={navbar.title} items={navbar.items}/>
       <main>{children}</main>
     </>
   )
--- property-management/components/navbar.tsx
+++ property-management/components/navbar.tsx
@@ -1,9 +1,47 @@
+import { SvgIconComponent } from "@mui/icons-material";
+import { AppBar, Box, Divider, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, SvgIconTypeMap, Toolbar, Typography } from "@mui/material";
 
-
-export default function Navbar() {
+export interface NavbarProps {
+  title: string,
+  items: {
+    text: string,
+    link: string,
+    Icon: SvgIconComponent
+  }[];
+}
+export default function Navbar(props: NavbarProps) {
+    const drawerWidth = 240;
     return (
       <>
-        <h1>Navbar</h1>
+              <AppBar position="sticky" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            {props.title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+        }}
+      >
+        <Toolbar />
+        <Box sx={{ overflow: 'auto' }}>
+          <List>
+            {props.items.map((item, index) => (
+              <ListItem button key={item.text}>
+                <ListItemIcon>
+                  <item.Icon/>
+                </ListItemIcon>
+                <ListItemText primary={item.text} />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
       </>
     )
   }
