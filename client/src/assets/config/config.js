7d7e3434550d4eee19b0e32bafcffde7bc202846
severingafner <severingafner@gmail.com>
2022-05-03 21:56:55 +0200

    Define rows per page in a table globaly

--- client/src/assets/config/config.js
+++ client/src/assets/config/config.js
@@ -4,8 +4,7 @@ const envVarsSchema = Joi.object()
   .keys({
     NODE_ENV: Joi.string().valid('production', 'development', 'demo', 'test').required(),
     REACT_APP_API_URL: Joi.string().allow('').default('').description('backend server url'),
-    REACT_APP_STRIPE_PUBLIC_KEY: Joi.string().description('Stripe public key'),
-    REACT_APP_USERS_PER_PAGE: Joi.number().default(10).description('number of users per page in users table'),
+    REACT_APP_ROWS_PER_PAGE: Joi.number().default(10).description('number of users per page in users table'),
   })
   .unknown();
 
@@ -19,10 +18,7 @@ export const config = {
   api: {
     url: envVars.NODE_ENV === 'production' ? envVars.REACT_APP_API_URL : '',
   },
-  stripe: {
-    publicKey: envVars.REACT_APP_STRIPE_PUBLIC_KEY,
-  },
-  users: {
-  	resultsPerPage: envVars.REACT_APP_USERS_PER_PAGE,
+  table: {
+  	resultsPerPage: envVars.REACT_APP_ROWS_PER_PAGE,
   },
 };
