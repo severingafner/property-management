03bb9e0e4de787a662eaaa13b99618cac2cbfeca
severingafner <severingafner@gmail.com>
2022-05-03 21:59:08 +0200

    Create a generic form componet (textfield only)

---
+++ client/src/components/Forms/Form.js
@@ -0,0 +1,58 @@
+import React from "react"
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+import { Input, Label, Select, HelperText } from '@windmill/react-ui'
+
+function BaseForm({fields, submitCb, formRef, callback}) {
+  const initialValues = {}
+  const validationSchema = {};
+  fields.forEach((field) => {
+    initialValues[field.name] = field.default;
+    validationSchema[field.name] = field.validation
+  });
+  return (
+    <Formik
+      innerRef={formRef}
+      initialValues={initialValues}
+      validationSchema={Yup.object().shape(validationSchema)}
+      onSubmit={(data, { setStatus, setSubmitting }) => {
+        setStatus();
+        setTimeout(async () => {
+          await submitCb(data)
+          .then(
+            response => {
+              callback(true);
+            },
+            error => {
+              if(error.response) {
+                setStatus(error.response.data.message);
+              } else {
+                setStatus('Some error occured.');
+              }
+              callback(false);
+            }
+          );
+        }, 400);
+      }}
+    >  
+      {({ errors, status, touched, isSubmitting }) => (
+        <Form>
+          {fields.map((field) => 
+            <Label className="mt-4" key={field.name}>
+              <span>{field.name.charAt(0).toUpperCase() + field.name.slice(1)}</span>
+              <Field className="mt-1" as={Input} name={field.name} type={field.type || 'text'}/>
+              <ErrorMessage name={field.name}>{msg => <HelperText valid={false}>{msg}</HelperText>}</ErrorMessage>
+           </Label>
+          )}
+
+          {status && (
+            <HelperText valid={false}>{status}</HelperText>
+          )}
+          
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+export default BaseForm
